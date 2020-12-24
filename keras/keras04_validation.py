6ecd7471a3e51e1c389a8987a6465a1e92088d07
DESKTOP-BB15P9S\Administrator <kimsuhyun72@Naver.com>
2020-12-24 13:56:53 +0900

    12/23 오전

--- keras/keras04_validation.py
+++ keras/keras04_validation.py
@@ -5,8 +5,14 @@ from tensorflow.keras.layers import Dense
 
 x_train = np.array([1,2,3,4,5])
 y_train = np.array([1,2,3,4,5])
-x_test = np.array([6,7,8])
-y_test = np.array([6,7,8])
+
+x_validation = np.array([6,7,8])
+y_validation = np.array([6,7,8])
+
+x_test = np.array([9,10,11])
+y_test = np.array([9,10,11])
+
+
 
 model = Sequential()
 model.add(Dense(5,input_dim = 1,activation='relu'))
@@ -15,7 +21,7 @@ model.add(Dense(4))
 model.add(Dense(1))
 #model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
 model.compile(loss='mse',optimizer='adam',metrics=['mae'])
-model.fit(x_train,y_train,epochs=1000,batch_size=1)
+model.fit(x_train,y_train,validation_data = (x_validation,y_validation), epochs=1000, batch_size=1)
 
 loss = model.evaluate(x_test,y_test,batch_size=1)
 print('loss : ',loss)
