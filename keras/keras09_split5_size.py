7d38f12f7ffbbdd268592b3197e3493f0407377a
DESKTOP-BB15P9S\Administrator <kimsuhyun72@Naver.com>
2020-12-28 17:38:17 +0900

    12/28
    split 함수 분석내용

--- keras/keras09_split5_size.py
+++ keras/keras09_split5_size.py
@@ -15,9 +15,26 @@ y = np.array(range(1,101))
 # y_test = y[80:]
 
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y,train_size=0.8,shuffle=True)
-x_train,x_val,y_train,y_val = train_test_split(x_train,y_train,train_size=0.8)
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size=0.7,test_size=0.2,shuffle=False)
+#x_train,x_val,y_train,y_val = train_test_split(x_train,y_train,train_size=0.8)
 
+print('train size : ',x_train.shape,y_train.shape)
+print('test size : ',x_test.shape,y_test.shape)
+print(x_test)
+#print('validation size : ',x_val.shape,y_val.shape)
+'''
+하나의 파라미터만 있을경우 나머지를 모두 배정하는식
+둘다 있는데 1보다 크면 오류 1이면 위와같음
+1보다 
+train size :  (70,) (70,)
+test size :  (20,) (20,)
+'''
+
+
+
+
+
+'''
 model = Sequential()
 model.add(Dense(100,input_dim=1))
 model.add(Dense(50,activation='linear'))
@@ -37,3 +54,4 @@ print('loss : ',loss, '\nmse : ',mae,'\nR2 : ',r2)
 #val o loss :  2.2395182895706967e-05 mse :  0.004373359493911266
 
 #from sklearn.metrics import mean_squared_error
+'''
