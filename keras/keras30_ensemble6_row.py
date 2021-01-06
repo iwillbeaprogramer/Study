ecf1b49e6de9d28f62803faa79f9c690c7e71f09
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-01-06 15:29:28 +0900

    1/6
    LSTM ensemble commit

---
+++ keras/keras30_ensemble6_row.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+x1=np.array([[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9],[8,9,10],[9,10,11],[10,11,12]])#,[20,30,40],[30,40,50],[40,50,60]]).reshape(13,3,1)
+x2=np.array([[10,20,30],[20,30,40],[30,40,50],[40,50,60],[50,60,70],[60,70,80],[70,80,90],[80,90,10],[90,100,110],[100,110,120],[2,3,4],[3,4,5],[4,5,6]]).reshape(13,3,1)
+y1=np.array([4,5,6,7,8,9,10,11,12,13])#,50,60,70])
+y2=np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
+x1_predict = np.array([55,65,75]).reshape(1,3,1)
+x2_predict = np.array([65,75,85]).reshape(1,3,1)
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import LSTM,Dense,Input,concatenate
+
+inputs1 = Input(shape=(3,1))
+lstm1 = LSTM(16,activation='relu')(inputs1)
+dense1 = Dense(16,activation='relu')(lstm1)
+dense1 = Dense(8,activation='relu')(dense1)
+inputs2 = Input(shape=(3,1))
+lstm2 = LSTM(32,activation='relu',return_sequences=True)(inputs2)
+lstm2 = LSTM(16,activation='relu')(lstm2)
+dense2 = Dense(16,activation='relu')(lstm2)
+dense2 = Dense(8,activation='relu')(dense2)
+
+concat = concatenate([dense1,dense2])
+dense = Dense(4)(concat)
+outputs1 = Dense(1)(dense)
+outputs2 = Dense(1)(dense)
+
+model = Model([inputs1,inputs2],[outputs1,outputs2])
+model.compile(loss='mse',optimizer='adam')
+model.fit([x1,x2],[y1,y2],epochs=1000,batch_size=1)
+'''
+loss = model.evaluate([x1,x2],[y1,y2],batch_size=1)
+y1_pred = model.predict([x1_predict,x2_predict])
+print(y1_pred)
+print('loss : ',loss)
+'''
+# predict 는 85근사치
+'''
+[[85.69777]]
+loss :  0.007422684691846371
+'''
--- keras/test123.py
+++ keras/test123.py
@@ -1,43 +1,68 @@
-# keras23_LSTM3_scale 을 DNN으로 코딩
-# 결과치 비교
 
-# DNN으로 23번 파일보다 loss를 좋게 만들 것
-
+#1.데이터 구성
 import numpy as np
+from numpy import array 
 
-x = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6],
-              [5,6,7], [6,7,8], [7,8,9], [8,9,10],
-              [9,10,11], [10,11,12],
-              [20,30,40], [30,40,50], [40,50,60]])
-y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70]) 
+x1=array([[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9],[8,9,10],[9,10,11],[10,11,12],[20,30,40],[30,40,50],[40,50,60]])
+x2=array([[10,20,30],[20,30,40],[30,40,50],[40,50,60],[50,60,70],[60,70,80],[70,80,90],[80,90,100],[90,100,110],[100,110,120],[2,3,4],[3,4,5],[4,5,6]])
 
-x_pred = np.array([50,60,70])
+y=array([4,5,6,7,8,9,10,11,12,13,50,60,70]) 
 
-print(x.shape)      # (13, 3)
-print(y.shape)      # (13,)
+x1_predict=array([55,65,75])
+x2_predict=array([65,75,85])
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+print("x1.shape:",x1.shape) #(13,3)
+print("x2.shape:",x2.shape) #(13,3)
+print("y.shape:",y.shape) #(13,)
 
-model = Sequential()
-model.add(Dense(1024, activation='relu', input_shape=(3,)))
-model.add(Dense(512, activation='relu'))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(1))
+x1=x1.reshape(x1.shape[0],x1.shape[1],1)
+x2=x2.reshape(x2.shape[0],x2.shape[1],1)
+print(x1.shape) #(13, 3, 1)
+print(x2.shape) #(13, 3, 1)
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=1, batch_size=1, verbose=2)
+#2. 모델 구성 
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import LSTM,Dense,Input 
 
-loss = model.evaluate(x, y)
-print('loss :', loss)
-y_pred = model.predict(np.array([x[0]]))
-print('y_pred :', y_pred)
+#모델1
+input1=Input(shape=(3,1)) 
+dense1=LSTM(10,activation='relu')(input1) 
+dense1=Dense(10, activation='relu')(dense1)
 
-# loss : 0.0026478709187358618
-# y_pred : [[80.69948]]
+#모델2
+input2=Input(shape=(3,1)) 
+dense2=LSTM(10,activation='relu')(input2)
+dense2=Dense(10, activation='relu')(dense2)
+
+#모델병합concatenate
+from tensorflow.keras.layers import concatenate
+merge1=concatenate([dense1,dense2])
+
+#중간층 모델구성:생략가능
+middle1=Dense(15)(merge1)
+middle1=Dense(15)(middle1)
+
+#모델 분기1
+output1=Dense(30)(middle1)
+output1=Dense(7)(output1)
+output1=Dense(3)(output1)
+
+#모델 선언
+model=Model(inputs=[input1,input2], outputs=output1)
+
+#3.컴파일, 훈련
+model.compile(loss='mse',optimizer='adam',metrics='mae')
+model.fit([x1,x2],y,epochs=10,verbose=1) #배치사이즈 넣으면 에러뜸,왜?
+
+#4.평가, 예측
+result=model.evaluate([x1,x2],y)  
+print(result)
+
+x1_pred=x1_predict.reshape(1,3,1) 
+x2_pred=x2_predict.reshape(1,x2.shape[1],1)
+print(x1_pred.shape) #(1, 3, 1)
+print(x2_pred.shape) #(1, 3, 1)
+
+
+y_pred=model.predict([x1_pred,x2_pred]) #뒤에 
+print(y_pred) #값 8.5 근사값 1개가 나와야 하는데 [[ 3.9641004 14.315914   9.244208 ]] 3개가 나옴
