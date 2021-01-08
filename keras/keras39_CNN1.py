33fd7d95210d66263f3094a8662f10d91d3dde15
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-01-08 16:40:19 +0900

    1/8

--- keras/keras39_CNN1.py
+++ keras/keras39_CNN1.py
@@ -1,8 +1,9 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D,Dense,Flatten
+from tensorflow.keras.layers import Conv2D,Dense,Flatten,MaxPooling2D
 
 model = Sequential()
-model.add(Conv2D(filters=10,kernel_size=(2,2),padding='same',input_shape = (10,10,3),strides=2))  # ->> 넘어가면 (9,9,10)
+model.add(Conv2D(filters=10,kernel_size=(2,2),padding='same',input_shape = (10,10,3),strides=1))  # ->> 넘어가면 (9,9,10)
+model.add(MaxPooling2D(pool_size=(2,3)))
 model.add(Conv2D(9,(2,2)))
 # model.add(Conv2D(9,(2,3)))
 # model.add(Conv2D(8,2))
