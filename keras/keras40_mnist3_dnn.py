9a8f4211aab23469d14ef36c264b2b834d398bac
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-01-09 13:37:14 +0900

    1/9
    mnist4_LSTM

--- keras/keras40_mnist3_dnn.py
+++ keras/keras40_mnist3_dnn.py
@@ -4,24 +4,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-from tensorflow.keras.datasets import mnist
-
-(x_train,y_train),(x_test,y_test) = mnist.load_data()
-
-x_train = x_train.reshape(60000,28*28).astype('float32')/255.
-x_test = x_test.reshape(-1,28*28)/255.
-# (x_test.reshape(x_test[0],x_test.shape[1],x_test.shape[2],1))
-
 from tensorflow.keras.utils import to_categorical
-
-
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D,MaxPooling2D,Dense,Flatten,Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.datasets import mnist
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
 
 es = EarlyStopping(monitor='loss',patience=10)
+x_train = x_train.reshape(60000,28*28).astype('float32')/255.
+x_test = x_test.reshape(-1,28*28)/255.
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
@@ -48,17 +40,11 @@ print('accuracy : ',loss[1])
 
 #0.985 이상
 '''
+CNN
 313/313 [==============================] - 2s 5ms/step - loss: 0.0931 - accuracy: 0.9874
-실제 :  7 예상 :  7
-실제 :  2 예상 :  2
-실제 :  1 예상 :  1
-실제 :  0 예상 :  0
-실제 :  4 예상 :  4
-실제 :  1 예상 :  1
-실제 :  4 예상 :  4
-실제 :  9 예상 :  9
-실제 :  5 예상 :  5
-실제 :  9 예상 :  9
 accuracy :  0.9873999953269958
+
+DNN
+accuracy :  0.9724000096321106
 '''
 
