2401920204b10b47258a5a286e46ff07a67cc065
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-01-13 14:35:01 +0900

    1/13
    commit

---
+++ keras/keras53_pandas3_read_csv.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("../data/csv/iris_sklearn.csv",index_col=0,header=0)
+
+print(df)
---
+++ keras/keras53_pandas4_np.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("../data/csv/iris_sklearn.csv",index_col=0,header=0)
+
+print(df)
+array = df.values
+print(array)
