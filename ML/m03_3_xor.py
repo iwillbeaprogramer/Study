6b84e62b81d201c4501cda1973373a7a9fb320e5
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-01-27 18:11:35 +0900

    1/27

---
+++ ML/m03_3_xor.py
@@ -0,0 +1,16 @@
+from sklearn.svm import LinearSVC
+import numpy as np
+from sklearn.metrics import accuracy_score
+x_data = np.array([[0,0],[0,1],[1,0],[1,1]])
+y_data = np.array([0,1,1,0])
+
+model = LinearSVC()
+
+model.fit(x_data,y_data)
+
+y_pred = model.predict(x_data)
+result = model.score(x_data,y_data)
+print(x_data,"의 예측결과 : ",y_pred)
+print('model.score : ',result)
+acc = accuracy_score(y_data,y_pred)
+print("acc : ",acc)
---
+++ ML/m03_4_xor2.py
@@ -0,0 +1,25 @@
+from sklearn.svm import LinearSVC,SVC
+import numpy as np
+from sklearn.metrics import accuracy_score
+x_data = np.array([[0,0],[0,1],[1,0],[1,1]])
+y_data = np.array([0,1,1,0])
+
+model = SVC()
+
+model.fit(x_data,y_data)
+
+y_pred = model.predict(x_data)
+result = model.score(x_data,y_data)
+print(x_data,"의 예측결과 : ",y_pred)
+print('model.score : ',result)
+acc = accuracy_score(y_data,y_pred)
+print("acc : ",acc)
+
+'''
+[[0 0]
+ [0 1]
+ [1 0]
+ [1 1]] 의 예측결과 :  [0 1 1 0]
+model.score :  1.0
+acc :  1.0
+'''
