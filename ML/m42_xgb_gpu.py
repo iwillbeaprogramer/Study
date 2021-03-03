614c61bbc7b039b0e5e146f48f6097abeb1ad3d7
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-03-03 23:36:36 +0900

    03/03

--- ML/m42_xgb_gpu.py
+++ ML/m42_xgb_gpu.py
@@ -9,7 +9,7 @@ x = datasets.data
 y = datasets.target
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
-model = XGBRegressor(n_estimators = 1000,learning_rate = 0.01,tree_method = 'gpu_hist')
+model = XGBRegressor(n_estimators = 1000,learning_rate = 0.01,tree_method = 'gpu_hist',predictor = 'gpu_predictor',gpu_id = 0)
 model.fit(x_train,y_train,verbose=1,eval_metric=['rmse'],eval_set = [(x_train,y_train),(x_test,y_test)],early_stopping_rounds=1000)
 
 score = model.score(x_test,y_test)
