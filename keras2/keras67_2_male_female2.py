8d574e4f7603d993c82fcd5f9a2c638a83aa3ba5
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-03-05 20:05:32 +0900

    03/05

--- keras2/keras67_2_male_female2.py
+++ keras2/keras67_2_male_female2.py
@@ -12,37 +12,35 @@ from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 # 1. 데이터
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True,
-    vertical_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    rotation_range=5,
-    zoom_range=1.2,
-    shear_range=0.7,
-    fill_mode='nearest',
-    validation_split=0.2
+    # rescale=1./255,
+    # horizontal_flip=True,
+    # vertical_flip=True,
+    # width_shift_range=0.1,
+    # height_shift_range=0.1,
+    # rotation_range=5,
+    # zoom_range=1.2,
+    # shear_range=0.7,
+    # fill_mode='nearest',
+    # validation_split=0.2
 )
-test_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2) 
+test_datagen = ImageDataGenerator() 
 
 # male - 841 (1) , female - 895 (0)
 xy_train = train_datagen.flow_from_directory(
-    '../data/image/human',
-    target_size=(150,150),
-    batch_size=1389,
+    '../data/image/male_female/male_female',
+    target_size=(224,224),
+    batch_size=1736,
     class_mode='binary',
-    shuffle=True,
     subset='training'
 )
 
-xy_val = train_datagen.flow_from_directory(
-    '../data/image/human',
-    target_size=(150,150),
-    batch_size=347,
-    class_mode='binary',
-    shuffle=False,
-    subset='validation'
-)
+# xy_val = train_datagen.flow_from_directory(
+#     '../data/image/male_female/male_female',
+#     target_size=(224,224),
+#     batch_size=347,
+#     class_mode='binary',
+#     # subset='validation'
+# )
 
 print(xy_train[0])
 print(xy_train[0][0])   
@@ -50,21 +48,21 @@ print(xy_train[0][0].shape)   # X -> (1389, 150, 150, 3)
 print(xy_train[0][1])  
 print(xy_train[0][1].shape)   # Y -> (1389,)
 
-print(xy_val[0])
-print(xy_val[0][0])   
-print(xy_val[0][0].shape)     # X -> (347, 150, 150, 3)
-print(xy_val[0][1])  
-print(xy_val[0][1].shape)     # Y -> (347,)
+#print(xy_val[0])
+#print(xy_val[0][0])   
+#print(xy_val[0][0].shape)     # X -> (347, 150, 150, 3)
+#print(xy_val[0][1])  
+#print(xy_val[0][1].shape)     # Y -> (347,)
 
-np.save('../data/image/brain/npy/k67_train_x.npy', arr=xy_train[0][0])
-np.save('../data/image/brain/npy/k67_train_y.npy', arr=xy_train[0][1])
-np.save('../data/image/brain/npy/k67_val_x.npy', arr=xy_val[0][0])
-np.save('../data/image/brain/npy/k67_val_y.npy', arr=xy_val[0][1])
+np.save('../data/image/brain/npy/k67_train_x__.npy', arr=xy_train[0][0])
+np.save('../data/image/brain/npy/k67_train_y__.npy', arr=xy_train[0][1])
+#np.save('../data/image/brain/npy/k67_val_x__.npy', arr=xy_val[0][0])
+#np.save('../data/image/brain/npy/k67_val_y__.npy', arr=xy_val[0][1])
 
 x_train = np.load('../data/image/brain/npy/k67_train_x.npy')
 y_train = np.load('../data/image/brain/npy/k67_train_y.npy')
-x_val = np.load('../data/image/brain/npy/k67_val_x.npy')
-y_val = np.load('../data/image/brain/npy/k67_val_y.npy')
+#x_val = np.load('../data/image/brain/npy/k67_val_x.npy')
+#y_val = np.load('../data/image/brain/npy/k67_val_y.npy')
 
 print(x_train.shape, y_train.shape)
 print(x_val.shape, y_val.shape)
---
+++ keras2/keras78_08_MobileNetV2.py
@@ -0,0 +1,45 @@
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.callbacks import EarlyStopping,ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import OneHotEncoder
+
+(x_train,y_train),(x_test,y_test) = cifar10.load_data()
+es = EarlyStopping(monitor = 'val_accuracy',patience=20)
+lr = ReduceLROnPlateau(monitor = 'val_accuracy',patience=7,factor = 0.6)
+optimizer = Adam(lr = 0.01)
+
+x_train = x_train.astype('float32')/255.
+x_test = x_test.astype('float32')/255.
+
+scaler = OneHotEncoder()
+y_train = scaler.fit_transform(y_train.reshape(-1,1)).toarray()
+y_test = scaler.transform(y_test.reshape(-1,1)).toarray()
+
+
+vgg16 = MobileNetV2(weights='imagenet',include_top = False, input_shape=(32,32,3))
+# include_top : True 면 imagenet에 들어갔던 weight를 그대로쓰는거야, False여야 우리가 원하는 사이즈로 변환해서 사용가능하다
+# print(model.weights)
+
+vgg16.trainable = False
+# vgg16.summary()
+# print(len(vgg16.weights))
+# print(len(vgg16.trainable_weights))
+
+model = Sequential()
+model.add(vgg16)
+model.add(Flatten())
+model.add(Dense(512,activation='relu'))
+model.add(Dense(10,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy',optimizer = optimizer,metrics = ['accuracy'])
+model.fit(x_train,y_train,validation_split=0.2,epochs=1000,batch_size=128,callbacks=[es,lr])
+
+result = model.evaluate(x_test,y_test)
+
+print("loss : ",result[0],"Accuracy : ",result[1])
+
+"""
+loss :  2.6307942867279053 Accuracy :  0.3199999928474426
+"""
