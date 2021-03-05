204effe4e792332706dc1a1dcaaed0b9ce9c32ab
DESKTOP-BB15P9S\Administrator <wisixicidi@gmail.com>
2021-03-05 20:09:14 +0900

    03/05

---
+++ keras2/keras78_01_cifar10_VGG19.py
@@ -0,0 +1,44 @@
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.callbacks import EarlyStopping,ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import OneHotEncoder
+
+(x_train,y_train),(x_test,y_test) = cifar10.load_data()
+es = EarlyStopping(monitor = 'val_accuracy',patience=20)
+lr = ReduceLROnPlateau(monitor = 'val_accuracy',patience=7,factor = 0.6)
+optimizer = Adam(lr = 0.01)
+
+x_train = x_train.astype('float32')/255.
+x_test = x_test.astype('float32')/255.
+
+scaler = OneHotEncoder()
+y_train = scaler.fit_transform(y_train.reshape(-1,1)).toarray()
+y_test = scaler.transform(y_test.reshape(-1,1)).toarray()
+
+
+vgg16 = VGG19(weights='imagenet',include_top = False,input_shape=(32,32,3))
+# include_top : True 면 imagenet에 들어갔던 weight를 그대로쓰는거야, False여야 우리가 원하는 사이즈로 변환해서 사용가능하다
+# print(model.weights)
+
+vgg16.trainable = False
+# vgg16.summary()
+# print(len(vgg16.weights))
+# print(len(vgg16.trainable_weights))
+
+model = Sequential()
+model.add(vgg16)
+model.add(Flatten())
+model.add(Dense(512,activation='relu'))
+model.add(Dense(10,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy',optimizer = optimizer,metrics = ['accuracy'])
+model.fit(x_train,y_train,validation_split=0.2,epochs=1000,batch_size=128,callbacks=[es,lr])
+
+result = model.evaluate(x_test,y_test)
+print(result)
+
+'''
+[1.497375249862671, 0.5819000005722046]
+'''
